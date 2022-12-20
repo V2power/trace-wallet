138bef49db13aca54dca9de62ad917df2835b459
V2power <vitor5088@gmail.com>
2022-12-20 20:52:42 -0300

    configurações inicias

--- src/index.js
+++ src/index.js
@@ -5,4 +5,23 @@ const app = express();
 
 app.use(express.json());
 
+const users = [];
+
 function checkExistsUserAccount(req, res, next) {}
+
+app.post("/wallets", (req, res) => {
+  const { ownerName } = req.body;
+
+  const user = {
+    id: uuidv4(),
+    name: ownerName,
+    limit: 0,
+    payments: [],
+  };
+
+  users.push(user);
+
+  return res.status(201).json(user);
+});
+
+module.exports = app;
